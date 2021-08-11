5e2f9b30e4442808f61b6e9e2391640c3d48d73b
carmolim <carmolim@gmail.com>
2021-08-11 18:35:48 -0300

    Updating the path subfolder

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    //   ? '/vuepothesis/'
-    //   : '/'
-    // ,
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vuepothesis/'
+      : '/'
+    ,
     chainWebpack: config => {
         config
           .plugin('html')
