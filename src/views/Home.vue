3b3702ea1714b204fc212993cefe4ffd008c6dd1
carmolim <carmolim@gmail.com>
2021-08-12 21:08:54 -0300

    Fixing some texts, removing some comments

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,25 +16,18 @@
                 class="underline hover:textDecoration"
                 href="https://hypothes.is/account/developer"
                 target="_blank" rel="noopener noreferrer">
-                generate and get
-            </a> your API Token on your
+                generate and get</a> your API Token on your
 
             <a
                 class="underline hover:textDecoration"
                 href="http://hypothes.is/" target="_blank"
                 rel="noopener noreferrer">
-                hypotes.is
-            </a>
-            account. 
+                hypotes.is</a> account. 
+            
         </p>
 
-        <!-- <p class="inline-flex items-stretch">
-            <a class="mb-2" href="https://hypothes.is/account/developer" target="_blank" rel="noopener noreferrer">Get your API key</a>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-            </svg>
-        </p> -->
-
+        <p class="mb-2">Please do make a <a class="underline hover:textDecoration" target="_blank" rel="noopener noreferrer" href="https://jonudell.info/h/facet">safe copy</a> your annotations first, and proceed with care.</p>
+        
         <form @submit.prevent="saveToken" class="d-md-flex">
 
             <input
@@ -161,7 +154,6 @@
                         class="w-full mb-5 border rounded-lg border-2 p-4 py-2"
                         type="password">
                         
-                    <!-- 6879-PUNP8MdCR_fSGZw-eG823k-EjJY3P1FH1241TSqVPIY -->
                     <input
                         class="mr-2 "
                         type="checkbox"
@@ -190,7 +182,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                    Change token info.
+                    Change the token info.
                 </p>
             
             </template>
@@ -241,7 +233,7 @@
 
         </div>
 
-        <div v-if="annotations">
+        <div class="w-full" v-if="annotations">
 
 	        <Annotation v-for="annotation in annotations"
                 :key="annotation.id"
